d96cb93e193fe349ac5072cdd19f15f6d9bf2e80
Jeff Ames <jeffames@gmail.com>
2012-10-03 22:06:08 -0400

    Fix emacs loading when ercpass file is not present

--- emacs.d/my-erc.el
+++ emacs.d/my-erc.el
@@ -1,26 +1,29 @@
-(require 'erc)
-(load "~/.ercpass")
-(require 'erc-services)
+(let ((ercpass-file "~/.ercpass"))
+  (if (file-exists-p ercpass-file)
+      (progn
+        (require 'erc)
+        (load ercpass-file)
+        (require 'erc-services)
 
-(erc-services-mode 1)
+        (erc-services-mode 1)
 
-(and
- (require 'erc-highlight-nicknames)
- (add-to-list 'erc-modules 'highlight-nicknames)
- (erc-update-modules))
+        (and
+         (require 'erc-highlight-nicknames)
+         (add-to-list 'erc-modules 'highlight-nicknames)
+         (erc-update-modules))
 
-(setq erc-prompt-for-nickserv-password nil)
+        (setq erc-prompt-for-nickserv-password nil)
 
-(setq erc-nickserv-passwords
-      `((freenode (("amesign" . ,freenode-amesign-pass)))))
+        (setq erc-nickserv-passwords
+              `((freenode (("amesign" . ,freenode-amesign-pass)))))
 
-(global-set-key "\C-cef" (lambda () (interactive)
-                           (erc :server "irc.freenode.net" :port "6667"
-                                :nick "amesign" :full-name "amesign")))
-(setq erc-autojoin-channels-alist '(("freenode.net"
-                                     "#emacs")))
+        (global-set-key "\C-cef" (lambda () (interactive)
+                                   (erc :server "irc.freenode.net" :port "6667"
+                                        :nick "amesign" :full-name "amesign")))
+        (setq erc-autojoin-channels-alist '(("freenode.net"
+                                             "#emacs")))
 
-(setq erc-interpret-mirc-color t)
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
+        (setq erc-interpret-mirc-color t)
+        (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT")))))
 
 (provide 'my-erc)
