7869e9b955a0544479610c5d715c1d13b158b09f
Jeff Ames <jeffames@gmail.com>
2012-10-03 22:00:51 -0400

    Only load package in emacs if emacs-version is at least 24

--- emacs.d/my-package.el
+++ emacs.d/my-package.el
@@ -1,7 +1,9 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" .
-               "http://marmalade-repo.org/packages/"))
-(package-initialize)
+(if (not (version< emacs-version "24"))
+    (progn
+      (require 'package)
+      (add-to-list 'package-archives
+                   '("marmalade" .
+                     "http://marmalade-repo.org/packages/"))
+      (package-initialize)))
 
 (provide 'my-package)
