623a3eb2174e209ea1a3f752bd80012b21f11b12
Jeff Ames <jeffames@gmail.com>
2012-12-19 14:06:04 -0500

    Workaround for xmonad not responding to inputs bug

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -15,11 +15,11 @@ main = do
          , focusedBorderColor = "#339933"
          , manageHook = manageDocks <+> manageHook defaultConfig
          , layoutHook = avoidStruts $ layoutHook defaultConfig
-         , logHook = dynamicLogWithPP xmobarPP
-           { ppOutput = hPutStrLn xmproc
-           , ppTitle = xmobarColor "blue" "" . shorten 50
-           , ppLayout = const "" -- to disable the layout info on xmobar
-           }
+--         , logHook = dynamicLogWithPP xmobarPP
+--           { ppOutput = hPutStrLn xmproc
+--           , ppTitle = xmobarColor "blue" "" . shorten 50
+--           , ppLayout = const "" -- to disable the layout info on xmobar
+--           }
          } `additionalKeys`
          [ ((mod4Mask .|. shiftMask, xK_c), spawn "chromium")
          , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
