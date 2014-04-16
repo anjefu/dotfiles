4cb84aa13ac93c114dec43ec95cc20dad7d72947
Jeff Ames <jeffames@gmail.com>
2014-04-15 21:53:28 -0400

    Tweak emacs themes

--- emacs.d/my-theme.el
+++ emacs.d/my-theme.el
@@ -2,8 +2,10 @@
 (unless (equal window-system nil)
     (progn
       (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;      (load-theme 'tango-dark)
+;      (load-theme 'tango-dark t)
       (load-theme 'tomorrow-night t)
+;      (load-theme 'tomorrow t)
+;      (load-theme 'zenburn t)
       ))
 
 (provide 'my-theme)
