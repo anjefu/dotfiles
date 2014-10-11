286b3117371dc0e2169dc635809dc5474f49040a
Jeff Ames <jeff@anjefu.com>
2014-10-11 17:13:05 -0400

    Add support for different configurations of ProofGeneral

--- emacs.d/my-proofgeneral.el
+++ emacs.d/my-proofgeneral.el
@@ -1,5 +1,8 @@
-(let ((proofgeneral "/usr/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el"))
-  (if (file-exists-p proofgeneral)
-      (load-file proofgeneral)))
+(let ((proofgeneral-1 "/usr/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+      (proofgeneral-2 "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el"))
+  (if (file-exists-p proofgeneral-1)
+      (load-file proofgeneral-1)
+    (if (file-exists-p proofgeneral-2)
+        (load-file proofgeneral-2))))
 
 (provide 'my-proofgeneral)
