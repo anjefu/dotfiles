3fb8504bc8e1c2497617cde95a2608d7e36c8fc1
Jeff Ames <jeff@anjefu.com>
2014-11-04 16:58:00 -0500

    Ensure some emacs packages are installed on startup

--- emacs.d/my-lisp/my-package.el
+++ emacs.d/my-lisp/my-package.el
@@ -4,6 +4,18 @@
       (add-to-list 'package-archives
                    '("marmalade" .
                      "http://marmalade-repo.org/packages/"))
-      (package-initialize)))
+      (package-initialize)
+
+      (when (not package-archive-contents)
+        (package-refresh-contents))
+
+      (defvar my-packages '(haskell-mode
+                            scala-mode
+                            sml-mode)
+        "A list of packages to ensure are installed at launch.")
+
+      (dolist (p my-packages)
+        (when (not (package-installed-p p))
+          (package-install p)))))
 
 (provide 'my-package)
