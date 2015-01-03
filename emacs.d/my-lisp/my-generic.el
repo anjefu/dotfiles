8b44004647e1e73443fc856339d28e96de1afd78
Jeff Ames <jeff@anjefu.com>
2015-01-03 10:22:49 -0500

    Add improved screen splitting for emacs

--- emacs.d/my-lisp/my-generic.el
+++ emacs.d/my-lisp/my-generic.el
@@ -58,6 +58,23 @@
 ; Alerts
 (setq visible-bell nil)
 
+; Screen splitting
+; from https://www.reddit.com/r/emacs/comments/25v0eo/you_emacs_tips_and_tricks/chldury
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(defun hsplit-last-buffer ()
+  (interactive)
+   (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
 ; Disabled commands
 (put 'narrow-to-region 'disabled nil)
 
